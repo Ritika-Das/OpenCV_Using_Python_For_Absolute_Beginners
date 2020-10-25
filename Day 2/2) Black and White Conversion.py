6d62548c71b6c766ab17b02fcb9b7a7b967b4724
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2020-10-25 12:00:27 +0530

    Update 2) Black and White Conversion.py

--- Day 2/2) Black and White Conversion.py
+++ Day 2/2) Black and White Conversion.py
@@ -1,9 +1,12 @@
-import cv2                                                  #
-import numpy as np                                          #
-import matplotlib.pyplot as plt                             #
+import cv2                                                  # importing the cv2 module i.e. computer vision, in python
+import numpy as np                                          # importing the numpy module i.e. numerical python, using an alias "np",
+                                                            # so that it is easier to type out
 
-img = cv2.imread('image.png',  cv2.IMREAD_GRAYSCALE)        #
+import matplotlib.pyplot as plt                             # importing the matplotlib.pyplot module using an alias "plt",
+                                                            # for displaying our resultant image in a window
 
-cv2.imshow('image', img)                                    #
-cv2.waitKey(0)                                              #
-cv2.destroyAllWindows()                                     #
+img = cv2.imread('image.png',  cv2.IMREAD_GRAYSCALE)        # getting the original image and converting it to grayscale/Black&White form
+
+cv2.imshow('image', img)                                    # displaying the final image in a new window
+cv2.waitKey(0)                                              # waits for delay = 0 milliseconds to handle another event on keypress by user
+cv2.destroyAllWindows()                                     # windows necessary to read/open photo are now closed or "destroyed"
