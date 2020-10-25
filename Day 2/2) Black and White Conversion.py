2c4cdefd49d9687ffd190d5bb0029f05264e4104
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2020-10-25 11:20:35 +0530

    Create 2) Black and White Conversion.py

---
+++ Day 2/2) Black and White Conversion.py
@@ -0,0 +1,9 @@
+import cv2                                                  #
+import numpy as np                                          #
+import matplotlib.pyplot as plt                             #
+
+img = cv2.imread('image.png',  cv2.IMREAD_GRAYSCALE)        #
+
+cv2.imshow('image', img)                                    #
+cv2.waitKey(0)                                              #
+cv2.destroyAllWindows()                                     #
